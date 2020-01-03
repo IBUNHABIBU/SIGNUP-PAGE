65fbceb415dfffbe14f1cc3bb901f8d76e4a1ce7
IBUNHABIB <ibunhabib@gmail.com>
2020-01-03 18:00:22 +0300

    adding has secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+    validates :name, presence: true, length: { minimum: 5, maximum: 50 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+    validates :email, presence: true, length: { minimum: 5, maximum: 255 },
+                      format: { with: VALID_EMAIL_REGEX }
+    has_secure_password
 end
